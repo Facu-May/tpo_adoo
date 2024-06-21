03df8daadb0f92bf6a445cf6e29ea460d15b7630
baufantauzzo <bautistafantauzzo@gmail.com>
2024-06-21 18:07:24 -0300

    las notificaciones no iban en Medico

--- src/implementacion/Medico.java
+++ src/implementacion/Medico.java
@@ -18,7 +18,6 @@ public abstract class Medico extends Persona{
         cita.setMedicoAsignado(this);
         cita.setCliente(paciente);
         cita.setEstado("Programada");
-        notificarPaciente(paciente, "Cita programada con el Dr. " + this.nombre + " " + this.apellido + " el " + cita.getFechaHora());
     }
 	
     public void realizarConsulta(TurnoMedico turno, String tratamiento) {
@@ -28,9 +27,5 @@ public abstract class Medico extends Persona{
         } else {
             System.out.println("El turno no está programado");
         }
-    }
-
-    private void notificarPaciente(Paciente paciente, String mensaje) {
-        System.out.println("Notificación enviada a " + paciente.getNombre() + ": " + mensaje);
     }
 }
